f965e596559517d498a1c0a7f484015cf1c45f87
justchen1369 <35386456+justchen1369@users.noreply.github.com>
2019-09-03 11:18:23 -0400

    Update cocktailShakerSort.js

--- algorithms/cocktailShakerSort.js
+++ algorithms/cocktailShakerSort.js
@@ -1,29 +1,28 @@
 "use strict";
 //variant of bubbleSort, go up then down.
 module.exports = (arr) => {
-  let sortedAndReady = false
-  while(!sortedAndReady){ //not sure what variable to use
-    let sorted = true     //and while true seems a bit like it could go wrong
-    for(let i=0; i < (arr.length); i++){
-    if (arr[i] > arr[i+1]) {
-      let temp = arr[i+1];
-      arr[i+1] = arr[i];
-      arr[i] = temp;
-      sorted = false
-    }
-    console.log(arr)
-    for(let i = arr.length; i > 0; i--){
-        if (arr[i] < arr[i-1]) {
-          let temp = arr[i-1];
-          arr[i-1] = arr[i];
-          arr[i] = temp;
-          sorted = false
-        }
-    }
-    console.log(arr)
-  }
-  if (sorted === true) {
-      return arr
-    }
-  }
+   let sortedAndReady = false
+   while(!sortedAndReady){ //not sure what variable to use
+      let sorted = true     //and while true seems a bit like it could go wrong
+      for(let i=0; i < (arr.length); i++){
+         if (arr[i] > arr[i+1]) {
+            let temp = arr[i+1];
+            arr[i+1] = arr[i];
+            arr[i] = temp;
+            sorted = false
+         }
+         for(let i = arr.length; i > 0; i--){
+            if (arr[i] < arr[i-1]) {
+               let temp = arr[i-1];
+               arr[i-1] = arr[i];
+               arr[i] = temp;
+               sorted = false
+            }
+         }
+         if(sorted){
+           sortedAndReady = true
+         }
+      }
+   }
+   return arr
 }
