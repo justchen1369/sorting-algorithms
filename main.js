ecd4be5afa4818ee9ce4dbebb598a44eae1389a4
justchen1369 <35386456+justchen1369@users.noreply.github.com>
2019-09-03 11:15:56 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 "use strict";
 let algorithms = require('require-all')(__dirname + '/algorithms')
 
-console.log(algorithms.cocktailShakerSort([4,3,3,2,1]))
-
-
-function newFunction() {
-    console.time('bubbleSort');
-    algorithms.bubbleSort(Array.from({ length: 1000 }, () => Math.floor(Math.random() * 98432)));
-    console.timeEnd('bubbleSort');
+for(let keys in algorithms) {
+  let array = [9,8,7,6,5,4,3,2,1]
+  let sortedArray = array.sort()
+  if(algorithms[keys](array) === sortedArray){
+    console.log('Sorting sucessful with ' + keys)
+  } else {
+    console.log(`ERROR! ALGORITHM ${keys} RETURNED ${algorithms[keys](array)}!`)
+  }
 }
