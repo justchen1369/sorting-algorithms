9f207e7651523617c8f126f0cf013f8993ff84be
justchen1369 <35386456+justchen1369@users.noreply.github.com>
2019-09-03 13:48:43 -0400

    Update bubbleSort.js

--- algorithms/bubbleSort.js
+++ algorithms/bubbleSort.js
@@ -1,17 +1,16 @@
 "use strict";
 module.exports = (arr) => {
-  while(true){
-    let sorted = true
-    for(let i=0; i < (arr.length); i++){
-    if (arr[i] > arr[i+1]) {
-      let temp = arr[i+1];
-      arr[i+1] = arr[i];
-      arr[i] = temp;
-      sorted = false
-    }
-  }
-  if (sorted === true) {
-      return arr
-    }
-  }
+   let swapped = true
+   do {
+      swapped = false
+      for(let i=0; i < (arr.length); i++){
+         if (arr[i] > arr[i+1]) {
+            let temp = arr[i+1];
+            arr[i+1] = arr[i];
+            arr[i] = temp;
+            swapped = true
+         }
+      }
+   } while (swapped)
+   return arr
 }
