3774ae28d2e15582bb25eb42c8e76c6adafd98e5
justchen1369 <35386456+justchen1369@users.noreply.github.com>
2019-09-03 11:55:44 -0400

    Update bubbleSort.js

--- algorithms/bubbleSort.js
+++ algorithms/bubbleSort.js
@@ -1,7 +1,7 @@
 "use strict";
 module.exports = (arr) => {
-  while(true){ //not sure what variable to use
-    let sorted = true     //and while true seems a bit like it could go wrong
+  while(true){
+    let sorted = true
     for(let i=0; i < (arr.length); i++){
     if (arr[i] > arr[i+1]) {
       let temp = arr[i+1];
