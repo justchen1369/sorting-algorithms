61841e303a8b8095fe1ce81b8d9df521ba457ea2
Your Name <you@example.com>
2019-09-04 20:54:45 -0400

    yeet

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 "use strict";
 let algorithms = require('require-all')(__dirname + '/algorithms')
 const {performance} = require('perf_hooks');
-
+let _cliProgress = require('cli-progress');
 for(let keys in algorithms) {
   let array = [9,8,7,6,5,4,3,2,1]
   if(algorithms[keys](array) === array.sort()){
@@ -12,15 +12,22 @@ for(let keys in algorithms) {
 }
 for(let key in algorithms) {
   let sortingtimes = [];
+  //create a progress bar and increment every test, which is the inner for loop
+  const progress_bar = new _cliProgress.SingleBar({}, _cliProgress.Presets.shades_classic);
+  progress_bar.start(1000, 0)
   for(let i = 0; i < 1000; i++) {
     let sortme = Array.from({length: 1000}, () => Math.floor(Math.random() * 40));
     let time = performance.now()
     algorithms[key](sortme);
     time = performance.now() - time
     sortingtimes.push(time);
+    progress_bar.increment()
   }
+  console.log() //make a new line for the average declaration
   let average = sortingtimes.reduce((a, b) => {
      return a + b; })/sortingtimes.length
   average = Math.floor(average);
   console.log(`${key} on average took ${average} milliseconds!`)
 }
+const wtf = require('wtfnode');
+wtf.dump();
